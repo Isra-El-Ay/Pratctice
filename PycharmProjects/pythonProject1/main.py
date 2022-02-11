84fb778fbf40916ab95d08066de8b5143941fe97
Isra-El-Ay <ayukreiziger1@gmail.com>
2022-02-11 10:56:28 -0500

    test project

---
+++ PycharmProjects/pythonProject1/main.py
@@ -0,0 +1,5 @@
+days = input("How many days make one week? ")
+week = input("How many hours in a day? ")
+minutes = 60 * int(week)
+
+print(f"They are {days} in a week and {week}hours in a day,which equals {minutes}minutes.")
